f68baaab216fa8bf7fbc3fe89297be70ff397afe
Yasmin Pereira <yasminbarroso0208@gmail.com>
2023-01-16 10:55:50 -0300

    Organo

---
+++ src/components/ListaSuspensa/index.js
@@ -0,0 +1,17 @@
+import "./ListaSuspensa.css"
+
+const ListaSuspensa = (props) => {
+    return (
+        <div className="lista-suspensa">
+            <label>{props.label}</label>
+            <select value={props.valor} onChange={e =>props.aoAlterado(e.target.value)} required={props.obrigatorio}>
+                <option value=""></option>
+                {props.itens.map(item => {
+                    return <option key={item}>{item}</option>
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default ListaSuspensa
