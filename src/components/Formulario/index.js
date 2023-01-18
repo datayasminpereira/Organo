e7c7bc003473313523206965a49b1d38bbb78c96
Yasmin Pereira <yasminbarroso0208@gmail.com>
2023-01-18 14:37:35 -0300

    Organo finalizado

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,5 +1,5 @@
 import "./Formulario.css"
-import CampoTexto from "../CampoTexto"
+import Campo from "../Campo"
 import ListaSuspensa from "../ListaSuspensa"
 import Botao from "../Botao"
 import { useState } from "react"
@@ -10,6 +10,8 @@ const Formulario = (props) => {
     const [cargo, setCargo] = useState('')
     const [imagem, setImagem] = useState('')
     const [time, setTime] = useState("")
+    const [nomeTime, setNomeTime] = useState('')
+    const [corTime, setCorTime] = useState('')
 
     const aoSalvar = (e) => {
         e.preventDefault()
@@ -26,23 +28,26 @@ const Formulario = (props) => {
     }
 
     return (
-        <section className="formulario">
-            <form onSubmit={aoSalvar}>
+        <section className="formulario-container">
+            <form className="formulario" onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto
+                <Campo
                     valor = {nome}
+                    type = "text"
                     aoAlterado = {valor => setNome(valor)}
                     obrigatorio={true}
                     label="Nome"
                     placeholder="Digite seu nome" />
-                <CampoTexto
+                <Campo
                     valor = {cargo}
+                    type = "text"
                     aoAlterado = {valor => setCargo(valor)}
                     obrigatorio={true}
                     label="Cargo" 
                     placeholder="Digite seu cargo" />
-                <CampoTexto 
+                <Campo 
                     valor = {imagem}
+                    type = "text"
                     aoAlterado = {valor => setImagem(valor)}
                     label="Imagem"
                     placeholder="Informe o endereço da imagem" />
@@ -55,6 +60,27 @@ const Formulario = (props) => {
                 <Botao>
                     Criar card
                 </Botao>
+            </form>
+            <form className="formulario" onSubmit={(evento) => {
+                evento.preventDefault()
+                props.aoCriarTime({ nome: nomeTime, cor: corTime })
+            }}>
+                <h2>Preencha os dados para criar um novo time.</h2>
+                <Campo
+                    type = "text"
+                    obrigatorio={true}
+                    label='Nome'
+                    placeholder='Digite o nome do time'
+                    valor={nomeTime}
+                    aoAlterado={valor => setNomeTime(valor)}/>
+                <Campo
+                    type = "color"
+                    obrigatorio={true}
+                    label='Cor' 
+                    placeholder='Digite sua cor'
+                    valor={corTime}
+                    aoAlterado={valor => setCorTime(valor)}/>
+                <Botao>Criar novo time</Botao>
             </form>
         </section>
     )
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,24 +1,33 @@
-import Colaborador from "../Colaborador"
 import "./Time.css"
+import Colaborador from "../Colaborador"
+
+import hexToRgba from "hex-to-rgba"
 
 const Time = (props) => {
 
-    const css = {
-        backgroundColor: props.corSecundaria
-    }
+    const css = { backgroundImage: "url(/images/fundo.png)", backgroundColor: hexToRgba(props.cor, 0.6) }
 
     return (
         (props.colaboradores.length > 0) ?
             <section className="time" style={css}>
-                <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+                <input value={props.cor} onChange={e => props.mudarCor(e.target.value, props.id)} type="color" className="input-cor"/>
+                <h3 style={{borderColor: props.cor}}>{props.nome}</h3>
                 <div className="colaboradores">
-                    {props.colaboradores.map(colaborador => 
-                        <Colaborador 
-                         key={colaborador.nome} 
-                         nome={colaborador.nome} 
-                         cargo={colaborador.cargo} 
-                         imagem={colaborador.imagem} 
-                         corDeFundo={props.corPrimaria}/>)}
+                    {props.colaboradores.map(colaborador =>  {
+                            return (
+                                <Colaborador 
+                                    id = {colaborador.id}
+                                    key={colaborador.nome} 
+                                    favorito = {colaborador.favorito}
+                                    nome={colaborador.nome} 
+                                    cargo={colaborador.cargo} 
+                                    imagem={colaborador.imagem} 
+                                    corDeFundo={props.cor} 
+                                    aoDeletar={props.aoDeletar}
+                                    aoFavoritar={props.aoFavoritar}/>
+                            )
+                        }
+                    )}
                 </div>
             </section>
         : ""
